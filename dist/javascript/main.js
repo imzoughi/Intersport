8cd85544e7dd2e0dc69a64f466344c37f6533f01
Issam-MZOUGHI <mzoughi.i@decade.fr>
2017-08-23 18:17:53 +0100

    Story #5056 [Page Saisir un ticket] Le champ "montant"

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -68,6 +68,9 @@ var responsiveBlock = function() {
             },
             focus: !1,
             disable: !1
+        }), $('input[data-type="number"]').on("input", function(evt) {
+            var self = $(this);
+            self.val(self.val().replace(/[^0-9\.]/g, "")), 46 == evt.which && self.val().indexOf(".") == -1 || !(evt.which < 48 || evt.which > 57) || evt.preventDefault();
         });
         var inputEmail = $("#user-email_id"), inputEmailValid = $("#user-autorisations-email-0_id"), inputMobile = $("#user-tel-mobile_id"), inputMobileValid = $("#user-autorisations-mobile-0_id");
         validationInput(inputEmail, inputEmailValid), validationInput(inputMobile, inputMobileValid);
--- javascript/main.js
+++ javascript/main.js
@@ -79,8 +79,8 @@ var bootstrapComponents = function () {
             responsive: true,
             autoWidth: true,
             searching: false,
-            paging:   true,
-            lengthMenu: [ [10, -1], [10, "Tout"] ],
+            paging: true,
+            lengthMenu: [[10, -1], [10, "Tout"]],
             pageLength: 10,
             lengthChange: false,
             ordering: true,
@@ -110,9 +110,18 @@ var formValidator = function () {
                 error: 'is-close'
             },
             focus: false,
-            disable: false
+            disable: false,
         });
 
+
+        $('input[data-type="number"]').on("input", function(evt) {
+            var self = $(this);
+            self.val(self.val().replace(/[^0-9\.]/g, ''));
+            if ((evt.which != 46 || self.val().indexOf('.') != -1) && (evt.which < 48 || evt.which > 57))
+            {
+                evt.preventDefault();
+            }
+        });
 
         // Autorisation Email et Mobile
         function validationInput(inputFiled, validField) {
